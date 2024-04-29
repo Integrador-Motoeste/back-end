3818ce8cc396a430f1fd5b99c16390618aed8812
MarcosFelintoLN <115835114+MarcosFelintoLN@users.noreply.github.com>
2024-04-29 16:43:09 -0300

    Crud de Avaliações (#5)
    * Crud de Avaliações
    
    * remove migrations
    
    * fixes
    
    ---------
    
    Co-authored-by: DamiaoFilho <damiaofilho0123@gmail.com>

--- config/api_router.py
+++ config/api_router.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
 from ride_app_back.users.api.views import UserViewSet, PilotViewSet
+from ride_app_back.ratings.api.viewsets import RatingViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
 router.register("pilots", PilotViewSet)
+router.register("ratings", RatingViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
