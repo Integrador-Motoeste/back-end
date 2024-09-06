fca5258bf50820cce6abd8a26d433ab37900e2fd
DamiaoFilho <damiaofilho0123@gmail.com>
2024-09-06 17:49:06 -0300

    partial queue comppleted

--- ride_app_back/rides/consumers.py
+++ ride_app_back/rides/consumers.py
@@ -1,15 +1,29 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class RideConsumer(AsyncWebsocketConsumer):
+class RideQueueConsumer(AsyncWebsocketConsumer):
     pilots = []  
 
-    async def connect(self):
-        user_type = self.scope['headers'][b'user-type'].decode('utf-8')
-        user_id = self.scope['headers'][b'user-id'].decode('utf-8')
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message_type = data.get('type')
 
-        self.scope['session']['user_type'] = user_type
+        if message_type == 'request_ride':
+            await self.request_ride(data)
+        if message_type == 'ride_request':
+            await self.ride_request(data)
+        if message_type == 'respond_ride':
+            await self.respond_ride(data)
+        if message_type == 'ride_response':
+            await self.ride_response(data)
+
+    async def connect(self):
+        user_id = self.scope['url_route']['kwargs']['user_id']
+        user_type = self.scope['url_route']['kwargs']['user_type']
+
         self.scope['session']['user_id'] = user_id
+        self.scope['session']['user_type'] = user_type
+
 
         if user_type == 'pilot':
             self.pilots.append(user_id)
@@ -29,13 +43,12 @@ class RideConsumer(AsyncWebsocketConsumer):
             self.pilots.remove(user_id)
             await self.channel_layer.group_discard('pilots', self.channel_name)
         else:
-            await self.channel_layer.group_discard(user_id, self.channel_name)
+            await self.channel_layer.group_discard(str(user_id), self.channel_name)
 
         print(f"{user_type} {user_id} desconectado")
 
     async def request_ride(self, event):
         passenger_id = event['passenger_id']
-        
         await self.channel_layer.group_send(
             'pilots',
             {
@@ -45,7 +58,9 @@ class RideConsumer(AsyncWebsocketConsumer):
         )
 
     async def ride_request(self, event):
+        print("ride_request")
         await self.send(text_data=json.dumps({
+            'type': 'ride_request',
             'passenger_id': event['passenger_id']
         }))
 
@@ -54,17 +69,19 @@ class RideConsumer(AsyncWebsocketConsumer):
         passenger_id = event['passenger_id']
         response = event['response']
 
-        await self.channel_layer.group_send(
-            passenger_id,
-            {
-                'type': 'ride_response',
-                'pilot_id': pilot_id,
-                'response': response
-            }
-        )
+        if response:
+            await self.channel_layer.group_send(
+                str(passenger_id),
+                {
+                    'type': 'ride_response',
+                    'pilot_id': pilot_id,
+                    'response': response
+                }
+            )
 
     async def ride_response(self, event):
         await self.send(text_data=json.dumps({
+            'type': 'ride_response',
             'pilot_id': event['pilot_id'],
             'response': event['response']
         }))
--- ride_app_back/rides/routes.py
+++ ride_app_back/rides/routes.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .consumers import RideConsumer
+from .consumers import RideQueueConsumer
 
 websocket_urlpatterns = [
-    path("ws/rides/", RideConsumer.as_asgi()),
+    path("ws/rides_queue/<str:user_id>/<str:user_type>/", RideQueueConsumer.as_asgi()),
 ]
