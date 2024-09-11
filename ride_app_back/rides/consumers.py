3c9d53c8db9643ec56625f037197496f2df79bd6
DamiaoFilho <damiaofilho0123@gmail.com>
2024-09-10 22:43:01 -0300

    add payment

--- ride_app_back/rides/consumers.py
+++ ride_app_back/rides/consumers.py
@@ -1,8 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from geopy.distance import geodesic
 class RideQueueConsumer(AsyncWebsocketConsumer):
-    pilots = []  
+    pilots = {} 
+    passengers = {}
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -22,6 +23,30 @@ class RideQueueConsumer(AsyncWebsocketConsumer):
             await self.pilot_confirmed(data)
         elif message_type == "user_not_found":
             await self.user_not_found(data)
+        elif message_type == "update_coords":
+            await self.update_coords(data)
+        elif message_type == "remove_coords":
+            await self.remove_coords(data)
+
+    async def update_coords(self, event):
+        user_id = event['user_id']
+        coords = event['coords']
+        user_type = event['user_type']
+
+        if user_type == 'pilot':
+            self.pilots[user_id] = coords
+        else:
+            self.passengers[user_id] = coords
+
+    async def remove_coords(self, event):
+        user_id = event['user_id']
+        user_type = event['user_type']
+
+        if user_type == 'pilot':
+            self.pilots.pop(user_id)
+        else:
+            self.passengers.pop(user_id)
+
 
     async def connect(self):
         user_id = self.scope['url_route']['kwargs']['user_id']
@@ -31,7 +56,6 @@ class RideQueueConsumer(AsyncWebsocketConsumer):
         self.scope['session']['user_type'] = user_type
 
         if user_type == 'pilot':
-            self.pilots.append(user_id)
             await self.channel_layer.group_add('pilots', self.channel_name)
 
         await self.channel_layer.group_add(user_id, self.channel_name)
@@ -54,18 +78,39 @@ class RideQueueConsumer(AsyncWebsocketConsumer):
 
     async def request_ride(self, event):
         passenger_id = event['passenger_id']
-        await self.channel_layer.group_send(
-            'pilots',
-            {
-                'type': 'ride_request',
-                'passenger_id': passenger_id
-            }
-        )
+        origin = event['origin']
+        destination = event['destination']
+        info = event['info']
+
+        passenger_point = (origin['latitude'], origin['longitude'])
+        nearby_pilots = []
+
+        for pilot_id, pilot_coords in self.pilots.items():
+            if pilot_coords:
+                pilot_point = (pilot_coords['latitude'], pilot_coords['longitude'])
+                distance = geodesic(passenger_point, pilot_point).km
+                if distance <= 5:
+                    nearby_pilots.append(pilot_id)
+
+        for pilot_id in nearby_pilots:
+            await self.channel_layer.group_send(
+                str(pilot_id),
+                {
+                    'type': 'ride_request',
+                    'passenger_id': passenger_id,
+                    'origin': origin,
+                    'destination': destination,
+                    'info': info
+                }
+            )
 
     async def ride_request(self, event):
         await self.send(text_data=json.dumps({
             'type': 'ride_request',
-            'passenger_id': event['passenger_id']
+            'passenger_id': event['passenger_id'],
+            'origin': event['origin'],
+            'destination': event['destination'],
+            'info': event['info']
         }))
 
     async def respond_ride(self, event):
@@ -129,7 +174,6 @@ class RideQueueConsumer(AsyncWebsocketConsumer):
 
 
 class RideExecutionConsumer(AsyncWebsocketConsumer):
-    users = []
 
     async def connect(self):
         ride_id = self.scope['url_route']['kwargs']['ride_id']
@@ -153,6 +197,10 @@ class RideExecutionConsumer(AsyncWebsocketConsumer):
             await self.send_change_pilot_position(data)
         elif message_type == 'confirm_boarding':
             await self.confirm_boarding(data)
+        elif message_type == 'finish_ride':
+            await self.finish_ride(data)
+        elif message_type == 'send_finish_ride':
+            await self.send_finish_ride(data)
 
     async def change_pilot_position(self, event):
         ride_id = event['ride_id']
@@ -188,4 +236,60 @@ class RideExecutionConsumer(AsyncWebsocketConsumer):
     async def send_confirm_boarding(self, event):
         await self.send(text_data=json.dumps({
             'type': 'confirm_boarding',
+        }))
+
+
+    async def finish_ride(self, event):
+        ride_id = event['ride_id']
+
+        await self.channel_layer.group_send(
+            str(ride_id),
+            {
+                'type': 'send_finish_ride',
+            }
+        )
+
+    async def send_finish_ride(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'finish_ride',
+        }))
+
+
+class PaymentConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        ride_id = self.scope['url_route']['kwargs']['ride_id']
+
+        await self.channel_layer.group_add(ride_id, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        ride_id = self.scope['url_route']['kwargs']['ride_id']
+
+        await self.channel_layer.group_discard(ride_id, self.channel_name)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message_type = data.get('type')
+
+        if message_type == 'confirmation':
+            await self.confirmation(data)
+        elif message_type == 'send_confirmation':
+            await self.send_confirmation(data)
+
+
+    async def confirmation(self, event):
+        ride_id = event['ride_id']
+
+        await self.channel_layer.group_send(
+            str(ride_id),
+            {
+                'type': 'send_confirmation',
+            }
+        )
+
+    async def send_confirmation(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'confirmation',
         }))
--- ride_app_back/rides/routes.py
+++ ride_app_back/rides/routes.py
@@ -4,4 +4,5 @@ from .consumers import RideQueueConsumer, RideExecutionConsumer
 websocket_urlpatterns = [
     path("ws/rides_queue/<str:user_id>/<str:user_type>/", RideQueueConsumer.as_asgi()),
     path("ws/rides/<str:ride_id>/", RideExecutionConsumer.as_asgi()),
+    path("ws/payments/<str:ride_id>/", RideExecutionConsumer.as_asgi()),
 ]
